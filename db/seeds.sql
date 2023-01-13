b2acb829984b63198b2eadfee659522cbf971af0
jessicashong <jessicashong12@gmail.com>
2023-01-12 19:03:50 -0600

    figuring out employee table seed

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,14 +1,17 @@
 INSERT INTO department (name)
-VALUES ('Human Resources'),
+VALUES ('Admin'),
+    ('Human Resources'),
     ('Marketing'),
-    ('Admin'),
     ('Tech');
 
 INSERT INTO role (title, salary, department_id)
-VALUES ('Customer Service Manager', '100000', 1),
-    ('Junior Developer', '80000', 4),
+VALUES ('CEO', '200000', 1),
+    ('Recruiter', '140000', 2),
     ('Senior Developer', '120000', 4),
-    ('Office Manager', '90000', 3);
+    ('Office Manager', '90000', 1);
 
--- INSERT INTO employee (first_name, last_name, role_id, manager_id)
--- VALUES ('Jess', 'Hong', 2)
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Michael', 'Scott', 4, 4),
+    ('Jim', 'Halpert', 3, 1),
+    ('Pam', 'Beesly', 2, 1)
+    -- ('David', 'Wallace', 1, null)
